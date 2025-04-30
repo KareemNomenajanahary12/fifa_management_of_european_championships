9915c6e7de458c2160727d119d613e25aa3a9833
KareeemNomenjanahary12 <hei.nomena.2@gmail.com>
2025-04-30 14:06:12 +0300

    feat: spring boot project initialization fifa_management_of_european_championships

---
+++ src/main/java/org/hei/school/fifa_management_if_european_championships/FifaManagementIfEuropeanChampionshipsApplication.java
@@ -0,0 +1,13 @@
+package org.hei.school.fifa_management_if_european_championships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaManagementIfEuropeanChampionshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifaManagementIfEuropeanChampionshipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hei/school/fifa_management_if_european_championships/FifaManagementIfEuropeanChampionshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.school.fifa_management_if_european_championships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaManagementIfEuropeanChampionshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
