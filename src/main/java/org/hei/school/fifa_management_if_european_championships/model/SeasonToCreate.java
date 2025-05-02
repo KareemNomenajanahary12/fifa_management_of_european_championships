34319f1a8c6961f7c779ace2ffeb202571822c3d
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-02 23:00:24 +0300

    feat(model): add SeasonToCreate DTO for season creation

---
+++ src/main/java/org/hei/school/fifa_management_if_european_championships/model/SeasonToCreate.java
@@ -0,0 +1,22 @@
+package org.hei.school.fifa_management_if_european_championships.model;
+
+public class SeasonToCreate {
+    private Integer year;
+    private String alias;
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
