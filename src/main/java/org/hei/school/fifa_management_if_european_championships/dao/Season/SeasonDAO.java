5f8a2ced117ee9365638220d020efa4962100685
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-03 19:04:14 +0300

    feat(dao): add findByYear and updateStatus method signatures

--- src/main/java/org/hei/school/fifa_management_if_european_championships/dao/Season/SeasonDAO.java
+++ src/main/java/org/hei/school/fifa_management_if_european_championships/dao/Season/SeasonDAO.java
@@ -1,11 +1,13 @@
 package org.hei.school.fifa_management_if_european_championships.dao.Season;
 
 import org.hei.school.fifa_management_if_european_championships.model.Season;
+import org.hei.school.fifa_management_if_european_championships.model.SeasonStatus;
 
 import java.util.List;
 
 public interface SeasonDAO {
     List<Season> getAll();
     List<Season> saveAll(List<Season> seasonsToCreate);
-    Season updateStatus(int year);
+    Season updateStatus(int year, SeasonStatus newStatus);
+    Season findByYear(int year);
 }
