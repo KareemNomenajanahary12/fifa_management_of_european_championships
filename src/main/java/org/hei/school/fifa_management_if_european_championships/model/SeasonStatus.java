027f79d2fc270665ad0b547d91c218b39cd0800d
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-03 19:15:15 +0300

    feat: add @JsonCreator method to SeasonStatus enum to support case-insensitive JSON values

--- src/main/java/org/hei/school/fifa_management_if_european_championships/model/SeasonStatus.java
+++ src/main/java/org/hei/school/fifa_management_if_european_championships/model/SeasonStatus.java
@@ -1,5 +1,12 @@
 package org.hei.school.fifa_management_if_european_championships.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum SeasonStatus {
-    NOT_STARTED,STARTED, FINISHED
+    NOT_STARTED,STARTED, FINISHED;
+
+    @JsonCreator
+    public static SeasonStatus parseEnumIgnoreCase(String key) {
+        return SeasonStatus.valueOf(key.toUpperCase());
+    }
 }
