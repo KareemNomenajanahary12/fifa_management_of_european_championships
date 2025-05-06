c53f46f2f248dab243128e8c7489572b443114aa
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-06 05:14:03 +0300

    feat : new SQL file to insert coaches in Liga

---
+++ src/main/java/org/hei/school/fifa_management_if_european_championships/dao/data/insert/insert_new_coaches.sql
@@ -0,0 +1,19 @@
+-- FC Barcelona
+INSERT INTO coaches (id, name, nationality, club_id)
+VALUES (uuid_generate_v4(), 'Hansi Flick', 'German', '74a36c94-7a1c-483c-ab6c-cea7c22cecf3');
+
+-- Atlético de Madrid
+INSERT INTO coaches (id, name, nationality, club_id)
+VALUES (uuid_generate_v4(), 'Diego Simeone', 'Argentine', '840b6712-f1b4-46d2-bbb9-a28c9091bb8d');
+
+-- Sevilla FC
+INSERT INTO coaches (id, name, nationality, club_id)
+VALUES (uuid_generate_v4(), 'Francisco García Pimienta', 'Spanish', 'c1fcfa1e-8fb3-4f70-a3a4-3f6e99e2ecc4');
+
+-- Valencia CF
+INSERT INTO coaches (id, name, nationality, club_id)
+VALUES (uuid_generate_v4(), 'Rubén Baraja', 'Spanish', 'a53cd87f-d5c0-42ef-b978-a3feacb461ab');
+
+-- Athletic Bilbao
+INSERT INTO coaches (id, name, nationality, club_id)
+VALUES (uuid_generate_v4(), 'Ernesto Valverde', 'Spanish', '436f2971-16ba-4d46-8b4f-60c8422e7aa9');
